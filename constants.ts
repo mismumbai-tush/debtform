63906db8dee89e6c2bcc01060a66653a21384956
mismumbai-tush <mis.mumbai@ginzalimited.com>
2026-01-27 12:49:11 +0530

    fix(constants): Correct Ulhasnagar spelling
    The spelling of "Ulasnagar" was corrected to "Ulhasnagar" in the BRANCH_SALES_MAPPING constant to ensure accurate data representation.

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ import { BranchData } from './types';
 
 export const BRANCH_SALES_MAPPING: BranchData = {
   "Mumbai": ["Vishal Ambhore","Amit Korgaonkar", "Santosh Pachratkar", "Rakesh Jain", "Kamlesh Sutar", "Pradeep Jadhav"],
-  "Ulasnagar": ["Sachin Bhosle","Shiv Ratan (Shivam)", "Viay Sutar"],
+  "Ulhasnagar": ["Sachin Bhosle","Shiv Ratan (Shivam)", "Viay Sutar"],
   "Kolkata": ["Rajesh Jain"],
   "Jaipur": ["Durgesh Bhati"],
   "Ludhiana": ["Mahesh Chandeliya"],
